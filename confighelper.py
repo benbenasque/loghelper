122f55512a3f3765b0345ad77327e7dd76e1de62
Sam Hawkins <sam.hawkins@vattenfall.com>
2014-08-05 18:26:41 +0200

    simplified confighelper.py and improved documentation

--- confighelper.py
+++ confighelper.py
@@ -1,24 +1,53 @@
-"""confighelper.py provides general configuration handling.
+"""confighelper.py provides general configuration handling. 
 
 Dependencies: json or yaml, docopt
 
-confighelper.py allows options to be specified in a json or yaml config file **and** overridden on the command line. 
-To use this module, import and call the config function passing in the calling module's docstring
-and command line arguments e.g.
+confighelper.py allows options to be specified in a json or yaml config file, specified by a --config=<file> option, 
+**and** allows options to be specified at the command line. File specified and command line specified options will be
+merged into a single dictionary, with none-null command-line options taking precedence and overriding the same specified
+within a file. Only those command-line arguments specified in the docstring can be given at the command line, while anything
+can be put into a configuration file. It is up to the user to keep the docstring up-to-date.
+
+Furthermore, configuration files can import other configuration files, can refer to environment variables and variables defined
+elsewhere in the configuration file.  
+
+To use this module, import and call the confighelper.config function,  passing in the calling module's docstring
+and command-line arguments e.g.
     
     mymodule.py
-    "mymodule.py docstring in docopt recognised format"
+    "mymodule.py docstring in docopt recognised format see docopt"
     
     import confighelper as conf
     config = conf.config(__doc__, sys.argv[1:] )
 
-Potential gotcha is that any defaults specified in the docstring will override those specified in the config file. 
-Therefore it is preferrable not to specify defaults in the docstring, but place them in a defaults config file. 
+If ** and only if ** a --config option is supplied at the command-line, this will be read and parsed, and merged with any 
+other command-line arguments. 
+    
+A potential "gotcha" is that any defaults specified using the docopt docstring will override those specified in the config file, 
+since they will be supplied as command-line arguments, even if they are not supplied at the command line.
+Therefore it is preferrable **not** to specify defaults in the docstring, but place them in a defaults config file. 
 
-Command-line options are stripped of preceding "--", to allow the equivalent 
-options to be specified in the config file without leading "--". 
+Command-line options are stripped of preceding "--", to allow the equivalent file options to be specified without a leading "--". 
 
-As an example, this module can be run from the command line:
+Expression expansion. Confighelper recognises three types of expression:
+ * environment variables using $(var) syntax
+ * configuration files using %[file] syntax
+ * local variables using %(var) syntax
+ 
+ A configuration file consists of (key, value) pairs. If a configuration file expression appears as a key, then it will
+ be read, parsed, and the original (key, value) pair will be replaced by the parsed dictionary.  In other words, the original 
+ value is treated as null, and the statement functions much like an import statement.  If a configuration file 
+ expression appears as a value, then it will be read, parsed, and the original value replaced by the parsed dictionary.
+
+The order of expansion is as follows:
+
+ 1. Parent configuration file is read as string
+ 2. Any environment variable expressions are expanded
+ 3. Any configuration file expressions are recursively loaded
+ 4. Any local variable expressions are expanded by lookup in the parent scope.
+ 
+ 
+This module itself can be run from the command line:
 
 Usage: 
     confighelper.py [--config=<file>]
@@ -51,7 +80,7 @@ YAML = ["yaml", "yml"]
 
 EVAR  = re.compile('\$\(.*?\)')                              # match rows containing environment vars
 LVAR  = re.compile('%\(.*?\)')                               # match rows containing local vars 
-CVAR = [re.compile('%\(.*\.'+e+'\)' ) for e in JSON + YAML]  # match rows containing config file specifiers
+CVAR = [re.compile('%\[.*\.'+e+'\]' ) for e in JSON + YAML]  # match rows containing config file specifiers
 
 
 class ConfigError(Exception):
@@ -70,7 +99,7 @@ def main():
     print json.dumps(c, indent=4, default=date_handler)
    
 
-def config(docstring, args, format="json", flatten=False):
+def config(docstring, args, format="json"):
     """ this is the primary function, which takes a modules's docstring, 
     a list of command line arguments (as in sys.argv[1:]) and returns a 
     merged dictionary which is the result of reading any file specified
@@ -81,18 +110,19 @@ def config(docstring, args, format="json", flatten=False):
     docstring -- calling module's docstring in docopt recognised format
     args      -- command line arguments as in sys.argv[1:]
     format    -- format for parsing configuration files (default json)
-    flatten   -- how to deal with hierachical inclusion of config files """
+    """
     
+    # parse command-line arguments using docopt
     cargs  = docopt.docopt(docstring,args)
         
     if '--config-fmt' in cargs and cargs['--config-fmt']:
         format = cargs['--config-fmt']
     
+    # if a config file is specified in the --config argument
     elif '--config' in cargs and cargs['--config']:
         format = cargs['--config'].split('.')[-1]
     
-    
-    
+        
     if format in JSON:
         import json
 
@@ -107,7 +137,7 @@ def config(docstring, args, format="json", flatten=False):
 
     # if a config file is specified, and is not 'falsy'
     if 'config' in cargs and cargs['config']:
-        cfile = load(cargs['config'], flatten=flatten)
+        cfile = load(cargs['config'])
         merged = merge(cargs, cfile)
         return merged
     else:
@@ -116,7 +146,7 @@ def config(docstring, args, format="json", flatten=False):
 
        
         
-def load(fname, evar=EVAR, lvar=LVAR, cvar=CVAR, flatten=False, format=None):
+def load(fname, evar=EVAR, lvar=LVAR, cvar=CVAR, format=None):
     """Loads a config file, and recursively opens any included config files 
     
     Arguments:
@@ -124,7 +154,6 @@ def load(fname, evar=EVAR, lvar=LVAR, cvar=CVAR, flatten=False, format=None):
     evar  -- regular expression matching environment variable definition
     lvar  -- regular expression matching local variable definition
     cvar  -- regular expression matching included config file  definition
-    flatten -- how to merge hierachical nested config files, see below
     format -- format to use for config file, json or yaml, default determine by file extension
     
     Suppose in the parent we have:
@@ -173,27 +202,38 @@ def load(fname, evar=EVAR, lvar=LVAR, cvar=CVAR, flatten=False, format=None):
     
     path, name = os.path.split(fname)
     
+    # open the filename and read as a string
     confstr = open(fname, 'r').read()
+    
+    # expand any environment variables into the string
     confstr = expand_evar(confstr, os.environ, evar)
     
+    # parse as json or yaml
     if format in JSON:
         parent=json.loads(confstr)
     
     if format in YAML:
         parent = yaml.load(confstr)
 
-        
+    # now walk through tree and expand any nested imports
     for key,value in parent.items():
-        if isinstance(value, (str, unicode)) and any(regex.match(value) for regex in cvar):
+        
+        # if key is a string, and matches a config file expression
+        if stringy(key) and any(regex.match(key) for regex in cvar):
+            subfname = key[2:-1]
+            subconf = load(path+"/"+subfname,evar, lvar, cvar, format=format)
+            # merge subconfiguration into parent scope
+            parent.update(subconf)
+            del(parent[key])
+        
+        # if value is a string, and values matches a config file expression
+        if stringy(value) and any(regex.match(value) for regex in cvar):
             subfname = value[2:-1]
-            subconf = load(path+"/"+subfname,evar, lvar, cvar, flatten=flatten, format=format)
-            if flatten:
-                parent.update(subconf)
-                del(parent[key])
-            else:
-                parent[key] = subconf
+            subconf = load(path+"/"+subfname,evar, lvar, cvar, format=format)
+            parent[key] = subconf
+            
 
-
+    # now expand any local variable definitions, i.e. nodes in the graph that refer to each other
     tree = parent.copy()
     scope = parent.copy()
     result = expand_tree(tree, scope, lvar)
@@ -232,8 +272,8 @@ def expand(string, scope, expr):
     return result
 
 def lookup(string, scope):
-    """ lookup a string within a dictionary and return result. If string not found, 
-    returns None """
+    """ lookup an expression string within a scope (dictionary) and return result. If string not found, 
+    returns None. Characters 0,1 and -1 are ignored e.g. %(key1) and $(key2) %[key3] become key1 and key2 and key3."""
     
     name = string[2:-1]
     if name in scope:
@@ -290,8 +330,6 @@ def merge(dict_1, dict_2):
     # values that are None do not override
     return dict((str(key), choose(key, dict_1, dict_2)) for key in set(dict_2) | set(dict_1))
     
-    return dict((str(key), dict_1.get(key) or dict_2.get(key))
-            for key in set(dict_2) | set(dict_1))
 
             
 def choose(key, dict1, dict2):
