c813f075b5ed2ae8d2cddf40f41d923c5405efc0
Sam Hawkins <samwisehawkins@gmail.com>
2015-09-04 12:48:34 +0200

    removed unecessary files from loghelper

--- loghelper.py
+++ loghelper.py
@@ -1,14 +1,23 @@
 import logging
+import inspect
 
-def create(name, log_level, log_fmt, log_file=None):
+DEFAULT_LEVEL = "INFO"
+DEFAULT_FMT = "%(message)s"
+
+
+
+def create(name, log_level=None, log_fmt=None, log_file=None):
 
     """ Creates a logging instance. If log_file is specified, handler is a
     FileHandler, otherwise StreamHandler"""
     
+    level = log_level if log_level else DEFAULT_LEVEL
+    fmt = log_fmt if log_fmt else DEFAULT_FMT
+    
     logger = logging.getLogger(name)    
-    numeric_level = getattr(logging, log_level.upper(), None)
+    numeric_level = getattr(logging, level.upper(), None)
     logger.setLevel(numeric_level)
-    formatter = logging.Formatter(log_fmt)    
+    formatter = logging.Formatter(fmt)    
     
     stream_handler = logging.StreamHandler()
     stream_handler.setLevel(numeric_level)
@@ -24,6 +33,20 @@ def create(name, log_level, log_fmt, log_file=None):
 
     return logger
     
+def file_handler(log_file, level, fmt):
+    numeric_level = getattr(logging, level.upper(), None)
+    formatter = logging.Formatter(fmt)    
+
+    log_handler = logging.FileHandler(log_file,mode='w')
+    log_handler.setLevel(numeric_level)
+    log_handler.setFormatter(formatter)
+    return log_handler
+
+
+
 
 def get(name):
     return logging.getLogger(name)
+
+def get2():
+    return logging.getLogger(inspect.stack()[-1][1])
